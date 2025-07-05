1385f9e159c2a6a9dfc7baad7126c9025a6862a7
Закиров Кирилл <k.zakirov@webbankir.ru>
2025-07-05 16:52:02 +0300

    footer mobile adaptation

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,14 +1,14 @@
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [NgFor, RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   standalone: true,
 })
 export class FooterComponent {
-
+  isNavOpen = false;
 }
