e92e0f06a969363974b2bf5e759d549e7e7e4cb7
Закиров Кирилл <k.zakirov@webbankir.ru>
2025-07-27 16:35:45 +0300

    fixes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,13 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { AuthActions } from '@app/states/auth/states/auth-actions';
 import { AuthState } from '@app/states/auth/states/auth.state';
 import { ChatActions } from '@app/states/chat/states/chat-actions';
 import { ChatState } from '@app/states/chat/states/chat.state';
 import { Store } from '@ngxs/store';
-import { Observable, of } from 'rxjs';
-
+import { distinctUntilChanged, filter, map, Observable } from 'rxjs';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-header',
   imports: [RouterLink, AsyncPipe],
@@ -17,17 +17,25 @@ import { Observable, of } from 'rxjs';
 })
 export class HeaderComponent {
   constructor(
-    private readonly store: Store
+    private readonly store: Store,
+    private router: Router,
+    private location: Location
   ) {
     this.currentUser$ = this.store.select(AuthState.login);
     this.isAuthorised$ = this.store.select(AuthState.isAuthorised);
     this.showChatWarning$ = this.store.select(ChatState.showWarning);
     this.isConnected$ = this.store.select(ChatState.isConnected);
+    this.showBackButton$ = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => /^\/products\/\d+/.test(this.router.url)), // Регулярка для `/products/186066`
+      distinctUntilChanged()
+    );
   }
 
   public readonly currentUser$: Observable<string | null>;
   public readonly isAuthorised$: Observable<boolean>;
   public readonly showChatWarning$!: Observable<boolean>;
+  public readonly showBackButton$!: Observable<boolean>;
 
   public readonly isConnected$: Observable<boolean>;
 
@@ -37,5 +45,9 @@ export class HeaderComponent {
 
   public toggleChat(): void {
     this.store.dispatch(new ChatActions.ToggleChatVisibility());
+  }
+
+  public goBack() {
+    this.location.back();
   }
 }
