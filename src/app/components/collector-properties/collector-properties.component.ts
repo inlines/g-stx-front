669ed2e70a04781323934bcb1894e3a0930cb99b
Закиров Кирилл <k.zakirov@webbankir.ru>
2025-12-06 14:26:36 +0300

    chat with collector button

--- src/app/components/collector-properties/collector-properties.component.ts
+++ src/app/components/collector-properties/collector-properties.component.ts
@@ -1,6 +1,7 @@
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ChatActions } from '@app/states/chat/states/chat-actions';
 import { ICollectionItem } from '@app/states/collection/interfaces/collection-item.interface';
 import { CollectorsState } from '@app/states/collectors/states/collectors.state';
 import { Store } from '@ngxs/store';
@@ -21,5 +22,14 @@ export class CollectorPropertiesComponent {
   ){
     this.loadedCollection$ = this.store.select(CollectorsState.loadedCollection);
     this.collectionPropertiesLogin$ = this.store.select(CollectorsState.collectionPropertiesLogin);
+  }
+
+  public startChatWith(): void {
+    const user = this.store.selectSnapshot(CollectorsState.collectionPropertiesLogin);
+    if(user) {
+      this.store.dispatch(new ChatActions.SetRecepient(user));
+      this.store.dispatch(new ChatActions.RequestMessages(user));
+      this.store.dispatch(new ChatActions.ToggleChatVisibility());
+    }
   }
 }
