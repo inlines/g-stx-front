2f6c4b4a50abdf62699b71e725475e026864016f
Закиров Кирилл <k.zakirov@webbankir.ru>
2026-07-11 13:28:51 +0300

    dev config localhost

--- src/app/environments/environment.dev.ts
+++ src/app/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 import { IEnvironment } from "./environment.interface";
 
 export const environment: IEnvironment = {
-  apiUrl: 'http://0.0.0.0:9090/api',
+  apiUrl: 'http://127.0.0.1:9090/api',
   wsUrl:  'ws://localhost:9090/ws/'
 }
