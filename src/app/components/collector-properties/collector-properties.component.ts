f6566f44ffbc7be1c7fc889c8c78e0fb0a14791a
Закиров Кирилл <k.zakirov@webbankir.ru>
2026-09-09 19:13:53 +0300

    more ava

--- src/app/components/collector-properties/collector-properties.component.ts
+++ src/app/components/collector-properties/collector-properties.component.ts
@@ -1,3 +1,4 @@
+import { UserAvatarComponent } from '../user-avatar/user-avatar.component';
 import { PlatformState } from '@app/states/platforms/states/platforms.state';
 import { CollectorsService } from '@app/states/collectors/services/collectors.service';
 import { unixMilliseconds } from '@app/shared/collection-filter';
@@ -16,7 +17,7 @@ import { PagerComponent } from '../pager/pager.component';
 import { ReleaseCardComponent } from '../release-card/release-card.component';
 @Component({
   selector: 'app-collector-properties',
-  imports: [RouterLink, AsyncPipe, PagerComponent, ReleaseCardComponent],
+  imports: [UserAvatarComponent, RouterLink, AsyncPipe, PagerComponent, ReleaseCardComponent],
   templateUrl: './collector-properties.component.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './collector-properties.component.scss',
